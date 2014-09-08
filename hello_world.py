a24f1a1104df3482ed7dc9e31fbd8ed88433c683
Ambreen <ambreen.ahmed@spsmail.cuny.edu>
2014-09-07 20:58:49 -0400

    Changed hello world to github username

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env  python
 # -*- coding: utf-8 -*-
 
-"""Changed to Github username ``Hello Aahmed`` statement."""
+"""Changed to Github username ``Hello"\w"Aahmed`` statement."""
 
 print "Hello_A_ahmed!"
+
+
+
+
+
+
+#
